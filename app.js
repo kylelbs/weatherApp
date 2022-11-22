6bab8445913483283656aa1f2eb69189438cbe17
kylelbs <kyle.lebrissaget@gmail.com>
2022-11-22 23:23:16 +0100

    added an input to let the user choose the city from his choice

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const APIKEY = 'b8b856db8fcd8ee1413fa747126666c1';
 let cityName = 'London';
+const mainDiv = document.querySelector('.content');
+const cityInput = document.querySelector('#city');
+const submitBtn = document.querySelector('#submit');
 
-async function getWeather (lat, lon) {
+async function getWeather (city) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKEY}`);
     const data = await response.json();
+    mainDiv.append(data.name);
     console.log(data);
+
 }
 
-getWeather(cityName);
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('clicked'),
+    cityName = cityInput.value;
+    getWeather(cityName);
+});
+
 

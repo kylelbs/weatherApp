798bfb860baaf4fcbe2f255205aba3e952e7e043
kylelbs <kyle.lebrissaget@gmail.com>
2022-11-23 21:18:54 +0100

    added an info display

--- app.js
+++ app.js
@@ -3,20 +3,60 @@ let cityName = 'London';
 const mainDiv = document.querySelector('.content');
 const cityInput = document.querySelector('#city');
 const submitBtn = document.querySelector('#submit');
+const answerContent = document.createElement('ul');
+answerContent.classList.add('answer-content');
 
 async function getWeather (city) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKEY}`);
+
+    try {    
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKEY}&units=metric`);
     const data = await response.json();
-    mainDiv.append(data.name);
-    console.log(data);
+    if (data.cod === '404') {
+        throw new Error('City not found');
+    } else {
+    
+    answerContent.innerHTML = '';
+
+    const cityLi = document.createElement('li')
+    cityLi.innerText = `City: ${data.name}`;
+    answerContent.append(cityLi);
+
+    const tempLi = document.createElement('li')
+    tempLi.innerText = `Temperature: ${data.main.temp}°C`;
+    answerContent.append(tempLi);
+
+    const feelsLikeLi = document.createElement('li')
+    feelsLikeLi.innerText = `Feels like: ${data.main.feels_like}°C`;
+    answerContent.append(feelsLikeLi);
+
+    const skyLi = document.createElement('li')
+    skyLi.innerText = `Sky: ${data.weather[0].main}`;
+    answerContent.append(skyLi);
+
+    mainDiv.append(answerContent);
+
+    console.log(data)};
+
+    } catch (error) {
+        console.log(error);
+        answerContent.innerHTML = '';
+        answerContent.append(error);
+    }
+
+    mainDiv.append(answerContent);
 
 }
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log('clicked'),
-    cityName = cityInput.value;
-    getWeather(cityName);
+    if (cityInput.value !== '') {
+        console.log('clicked'),
+        cityName = cityInput.value;
+        getWeather(cityName);
+    } else {
+        alert('Please enter a city name');
+    }
+
 });
 
 
